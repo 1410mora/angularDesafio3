c94e57405d1b01cf1c42c0d0c4eac736dd596b21
fercarrizoromero <fernando.maximiliano.carrizo@gmail.com>
2022-10-27 17:28:17 -0300

    incorporacion de componentes header, banner, acerca-de, experiencia, educacion, hys, proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienciaComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    HeaderComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
